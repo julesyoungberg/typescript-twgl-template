a1dcb39602b395a3fd66cd0eecfd1a1a60f19e7a
Jules Youngberg <julesdayoungberg@gmail.com>
2020-12-13 17:35:00 -0800

    update canvas id

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as twgl from 'twgl.js';
 const basicVertShader = require('./shaders/basic.vert');
 const basicFragShader = require('./shaders/basic.frag');
 
-const gl: WebGLRenderingContext = (document.getElementById('canvas') as any).getContext('webgl2');
+const gl: WebGLRenderingContext = (document.getElementById('webgl-canvas') as any).getContext('webgl2');
 const programInfo = twgl.createProgramInfo(gl, [basicVertShader, basicFragShader]);
 
 const arrays = {
